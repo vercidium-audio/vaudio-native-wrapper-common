79e5e26525cc70dbfd0d3bbbdedb4808d331cf77
Mitch RTX <mitchell@vercidium.com>
2026-08-18 11:54:31 +1000

    Moved Get/SetUseFlatTransmission binding to base primitive bindings

--- bindings/PrimitiveBindings.cs
+++ bindings/PrimitiveBindings.cs
@@ -10,5 +10,12 @@ namespace vaudionativewrapper
 
         [DllImport(Constants.DLL_NAME, CallingConvention = CallingConvention.Cdecl, EntryPoint = "vaPrimitiveGetMaterial")]
         public static extern MaterialType GetMaterial(IntPtr primitive);
+
+        [DllImport(Constants.DLL_NAME, CallingConvention = CallingConvention.Cdecl, EntryPoint = "vaPrimitiveSetUseFlatTransmission")]
+        public static extern VAResult SetUseFlatTransmission(IntPtr primitive, bool useFlatTransmission);
+
+        [DllImport(Constants.DLL_NAME, CallingConvention = CallingConvention.Cdecl, EntryPoint = "vaPrimitiveGetUseFlatTransmission")]
+        [return: MarshalAs(UnmanagedType.U1)]
+        public static extern bool GetUseFlatTransmission(IntPtr primitive);
     }
 }
